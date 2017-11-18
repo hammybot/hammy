3b4db285077770940391511cc23328b9c2d4fa16
Austin <austinvalle@gmail.com>
2017-11-18 01:22:02 -0500

    added !play ${youtube link}

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
 const Environment = require('dotenv');
+const ytdl = require('ytdl-core');
 
 Environment.config();
+
+const ytRegex = /^(!play )(https?\:\/\/)?((www\.)?youtube\.com|youtu\.?be)\/.+$/;
 
 const client = new Discord.Client();
 
@@ -20,6 +23,27 @@ client.on('message', message => {
     
     if(message.content === '!ping') {
         message.channel.send(':ping_pong: Pong!');
+    }
+
+    var ytUrl = message.content.match(ytRegex);
+    if(ytUrl && ytUrl[0]) {
+        if (!message.guild) return;
+
+        if(message.member.voiceChannel) {
+            message.member.voiceChannel.join().then(connection => {
+                const stream = ytdl(ytUrl[0], { filter : 'audioonly' });
+                const dispatcher = connection.playStream(stream);
+
+                dispatcher.on('end', () => {
+                    dispatcher.end();
+
+                    message.member.voiceChannel.leave();
+                });
+            })
+            .catch(console.log);
+        } else {
+            message.reply('You need to join a voice channel first.');
+        }
     }
 });
 
