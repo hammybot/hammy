f9c54dd94e0554e17d6b1090bce2db57ba9e2360
moosebot <austinvalle@gmail.com>
2019-09-06 23:22:04 -0400

    removed useless try/catches

--- src/modules/wato/db/wato-db.ts
+++ src/modules/wato/db/wato-db.ts
@@ -16,66 +16,38 @@ import {
 
 export const createNewChallenge = async (challenge: Challenge): Promise<void> => {
 	const pool = getDbPool();
-	try {
-		await pool.query(createNewChallengeSql(challenge));
-	} catch (error) {
-		throw error;
-	}
+	await pool.query(createNewChallengeSql(challenge));
 };
 
 export const getUserActiveChallenge = async (user: User): Promise<Challenge | null> => {
 	const pool = getDbPool();
-	try {
-		const result = await pool.query(getUserActiveChallengeSql(user));
-		if (result.rowCount === 0) { return null; }
+	const result = await pool.query(getUserActiveChallengeSql(user));
+	if (result.rowCount === 0) { return null; }
 
-		return result.rows[0];
-	} catch (error) {
-		throw error;
-	}
+	return result.rows[0];
 };
 
 export const setBetLimit = async (challenge: Challenge, betLimit: number): Promise<void> => {
 	const pool = getDbPool();
-	try {
-		await pool.query(setBetLimitSql(challenge, betLimit));
-	} catch (error) {
-		throw error;
-	}
+	await pool.query(setBetLimitSql(challenge, betLimit));
 };
 
 export const setChallengerBet = async (challenge: Challenge, bet: number): Promise<void> => {
 	const pool = getDbPool();
-	try {
-		await pool.query(setChallengerBetSql(challenge, bet));
-	} catch (error) {
-		throw error;
-	}
+	await pool.query(setChallengerBetSql(challenge, bet));
 };
 
 export const setChallengedBet = async (challenge: Challenge, bet: number): Promise<void> => {
 	const pool = getDbPool();
-	try {
-		await pool.query(setChallengedBetSql(challenge, bet));
-	} catch (error) {
-		throw error;
-	}
+	await pool.query(setChallengedBetSql(challenge, bet));
 };
 
 export const declineChallenge = async (challenge: Challenge): Promise<void> => {
 	const pool = getDbPool();
-	try {
-		await pool.query(updateChallengeStatusSql(challenge, ChallengeStatus.Declined));
-	} catch (error) {
-		throw error;
-	}
+	await pool.query(updateChallengeStatusSql(challenge, ChallengeStatus.Declined));
 };
 
 export const completeChallenge = async (challenge: Challenge, winnerId: string): Promise<void> => {
 	const pool = getDbPool();
-	try {
-		await pool.query(completeChallengeSql(challenge, winnerId));
-	} catch (error) {
-		throw error;
-	}
+	await pool.query(completeChallengeSql(challenge, winnerId));
 };
