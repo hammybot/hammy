31a32b794daeac77e0e94ca2b974eda945dc3912
moosebot <austinvalle@gmail.com>
2019-10-28 23:28:03 -0400

    add help msg in footer

--- src/modules/wato/wato-helper.ts
+++ src/modules/wato/wato-helper.ts
@@ -3,6 +3,8 @@ import { Client, GuildMember, RichEmbed, User } from 'discord.js';
 import { Challenge } from './models/challenge';
 import { ChallengeStatus } from './models/challenge-status';
 
+const QUESTION_MARK_ICON = 'https://i.imgur.com/DbxSPZy.png';
+
 const getChallengeStatusText = (challengeStatus: ChallengeStatus, challengedUser: User): string => {
 	switch (challengeStatus) {
 		case ChallengeStatus.Declined:
@@ -27,15 +29,20 @@ const getChallengeStatusColor = (challengeStatus: ChallengeStatus): string => {
 	}
 };
 
+const getHelpFooterText = (): string => {
+	return 'Need help? Just type !wato';
+};
+
 export const createWatoStatusEmbed = async (challenge: Challenge, client: Client): Promise<RichEmbed> => {
 	const challengerUser = await client.fetchUser(challenge.ChallengerId);
 	const challengedUser = await client.fetchUser(challenge.ChallengedId);
 
 	return new RichEmbed()
 		.setColor(getChallengeStatusColor(challenge.Status))
-		.setTitle(`\`${challengerUser.username}\` has challenged \`${challengedUser.username}\` to a game of odds!`)
+		.setTitle(`__${challengerUser.username}__ has challenged __${challengedUser.username}__ to a game of odds!`)
 		.setDescription(`\`\`\`${challenge.Description}\`\`\``)
-		.addField('**Status**', getChallengeStatusText(challenge.Status, challengedUser));
+		.addField('**Status**', getChallengeStatusText(challenge.Status, challengedUser))
+		.setFooter(getHelpFooterText(), QUESTION_MARK_ICON);
 };
 
 export const createWatoResultsEmbed = async (winnerId: string, challenge: Challenge, client: Client): Promise<RichEmbed> => {
@@ -51,7 +58,7 @@ export const createWatoResultsEmbed = async (winnerId: string, challenge: Challe
 
 	return new RichEmbed()
 		.setColor('#0099ff')
-		.setTitle(`\`${winningUser.username}\` has defeated \`${losingUser.username}\` in a game of odds!`)
+		.setTitle(`__${winningUser.username}__ has defeated __${losingUser.username}__ in a game of odds!`)
 		.setDescription(`\`\`\`${challenge.Description}\`\`\``)
 		.setThumbnail(`${winningUser.displayAvatarURL}`)
 		.addField(`${winningUser.username}'s bet`, `${winningBet}`, true)
@@ -62,12 +69,14 @@ export const createWatoDmEmbed = (username: string, betLimit: number): RichEmbed
 	return new RichEmbed()
 		.setColor('#ffffff')
 		.setTitle(`WATO Challenge - Bet Limit: ${betLimit}`)
-		.setDescription(`Place your bet for your odds challenge with ${username}`);
+		.setDescription(`Place your bet for your odds challenge with ${username}`)
+		.setFooter(getHelpFooterText(), QUESTION_MARK_ICON);
 };
 
 export const createWatoValidationEmbed = (errMessage: string): RichEmbed => {
 	return new RichEmbed()
 		.setColor('#ff2821')
 		.setTitle(`WATO Error`)
-		.setDescription(`${errMessage}`);
+		.setDescription(`${errMessage}`)
+		.setFooter(getHelpFooterText(), QUESTION_MARK_ICON);
 };
