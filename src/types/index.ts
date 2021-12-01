3c1408b45ee1a54c4fb4b7d628a7c0f1dfb9cf12
moosebot <austinvalle@gmail.com>
2021-12-01 00:54:51 -0500

    convert to new yarn and discord.ts

--- src/types/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './inversify-creators';
-export * from './symbols';
--- src/utils/message-combiner.ts
+++
@@ -1,9 +0,0 @@
-import { MessageHandlerPredicate } from '../models/message-handler';
-
-import { DiscordMessage } from './discord-message';
-
-export const combinePredicates = (...matchers: MessageHandlerPredicate[]): MessageHandlerPredicate => {
-	return (msg: DiscordMessage) => {
-		return matchers.every((matcher) => matcher(msg));
-	};
-};
