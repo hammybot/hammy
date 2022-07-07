3f9affed303f5e5445945129bfde804b275e38fe
Austin Valle <austinvalle@gmail.com>
2022-07-06 22:18:44 -0400

    setup main and default bot pkg

---
+++ cmd/hammy/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/austinvalle/hammy/pkg/bot"
+	"github.com/bwmarrin/discordgo"
+)
+
+const botTokenEnv = "DISCORD_BOT_TOKEN"
+
+func main() {
+	err := realMain()
+	if err != nil {
+		fmt.Printf("err: %v", err)
+		os.Exit(1)
+	}
+}
+
+func realMain() error {
+	botSession, err := createDiscordSession()
+	if err != nil {
+		return fmt.Errorf("failed creating discord client: %v", err)
+	}
+
+	err = bot.RunBot(botSession)
+	if err != nil {
+		return fmt.Errorf("failed running bot: %v", err)
+	}
+
+	return nil
+}
+
+func createDiscordSession() (*discordgo.Session, error) {
+	botToken := os.Getenv(botTokenEnv)
+	if botToken == "" {
+		return nil, fmt.Errorf("'%s' environment variable not found", botTokenEnv)
+	}
+
+	return discordgo.New(fmt.Sprintf("Bot %v", botToken))
+}
