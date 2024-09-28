deb99a857dd9f80dc7e1da5682f34f41d135ecfa
Austin Valle <austinvalle@gmail.com>
2024-09-27 22:55:16 -0400

    add challenge

---
+++ internal/bot/wato/db.go
@@ -0,0 +1,83 @@
+package wato
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type challengeStatus string
+
+const (
+	pendingAccept challengeStatus = "PND"
+	pendingBets   challengeStatus = "BET"
+	declined      challengeStatus = "DEC"
+	completed     challengeStatus = "CMP"
+)
+
+type challenge struct {
+	ID                 int
+	ChallengerID       string
+	ChallengedID       string
+	ChannelID          string
+	StatusMessageID    *string
+	Description        string
+	Status             challengeStatus
+	BetLimit           *int
+	WinnerID           *string
+	ChallengerBet      *int
+	ChallengedBet      *int
+	CreationTimestamp  time.Time
+	CompletedTimestamp *time.Time
+}
+
+func createNewChallenge(dbPool *pgxpool.Pool, c challenge) error {
+	insertQuery := `
+		INSERT INTO public.challenges (
+			"ChallengerId", "ChallengedId", "ChannelId", "Description", "Status"
+		)
+		VALUES (
+			$1, $2, $3, $4, $5
+		)
+	
+	`
+	_, err := dbPool.Exec(context.Background(), insertQuery, c.ChallengerID, c.ChallengedID, c.ChannelID, c.Description, c.Status)
+
+	return err
+}
+
+func getActiveChallenge(dbPool *pgxpool.Pool, userID string) (*challenge, error) {
+	getActiveChallengeQuery := `
+		SELECT * FROM public.challenges
+		WHERE ("ChallengerId" = $1 OR "ChallengedId" = $1)
+			AND "Status" NOT IN ('CMP', 'DEC');
+	`
+	rows, err := dbPool.Query(context.Background(), getActiveChallengeQuery, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := pgx.CollectOneRow(rows, func(row pgx.CollectableRow) (challenge, error) {
+		return pgx.RowToStructByNameLax[challenge](row)
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+func setStatusMessageID(dbPool *pgxpool.Pool, c challenge, msgID string) error {
+	updateQuery := `
+		UPDATE public.challenges
+		SET "StatusMessageId"= $1
+		WHERE "Id"= $2
+	
+	`
+	_, err := dbPool.Exec(context.Background(), updateQuery, msgID, c.ID)
+
+	return err
+}
